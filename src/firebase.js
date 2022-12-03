bf94fc534db99dbce40631f305231bac77a052fc
Jiwoncoding <seojiwonba@gmail.com>
2022-12-03 17:49:56 +0900

    firebase 설치 및 실행

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBz-DuEGkPWdSwc6dkmRXf6orzTqukdP7E",
+  authDomain: "nwitter-43462.firebaseapp.com",
+  projectId: "nwitter-43462",
+  storageBucket: "nwitter-43462.appspot.com",
+  messagingSenderId: "1013564271171",
+  appId: "1:1013564271171:web:d48a0bc8f4ea86c3c542f6",
+  measurementId: "G-QFX3MZB3NB"
+};
+
+export default firebase.initializeApp(firebaseConfig);
