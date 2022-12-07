04e18e63fe05666fcc2b6330d94ccf6047abca5f
Jiwoncoding <seojiwonba@gmail.com>
2022-12-08 07:59:51 +0900

    firebase로 로그인과 회원가입 처리하기

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState(""); 
   const [password, setPassword] = useState(""); 
+  const [newAccount, setNewAccount] = useState(true); // eslint-disable-line no-unused-vars
 
   const onChange = (event) => {
    const {
@@ -15,8 +17,21 @@ const Auth = () => {
    }
   };
 
-  const onSubmit = (event) => {
+  const onSubmit = async(event) => {
     event.preventDefault();
+    try {
+      let data;
+      if(newAccount){
+        // create newAccount
+        data = await authService.createUserWithEmailAndPassword(email, password);
+    } else {
+      // log in
+      data = await authService.signInWithEmailAndPassword(email, password);
+    }
+    console.log(data);
+  } catch(error){
+    console.log(error);
+  }
   };
 
   return(
@@ -24,7 +39,7 @@ const Auth = () => {
       <form onSubmit={onSubmit}>
         <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
