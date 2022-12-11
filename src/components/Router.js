4296426baa56f00ce43db59c16137e6318931a51
Jiwoncoding <seojiwonba@gmail.com>
2022-12-11 21:55:30 +0900

    Navigation 추가 및 로그아웃 처리하기

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,25 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
   return(
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
+          <>
           <Route path="/" element={<Home />} />
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          </>
         ) : (
           <Route path="/" element={<Auth />} />
         )}
+        {/* <Navigate to="/" /> */}
       </Routes>
     </Router>    // eslint-disable-line no-unused-vars
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,23 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "../fbase";
+// import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+
+  // const history = useHistory();
+  const navigate = useNavigate();
+
+  const onLogoutClick = () => {
+    authService.signOut();
+    // history.push("/");
+    navigate('/');
+  };
+
+  return (
+    <>
+    <button onClick={onLogoutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
