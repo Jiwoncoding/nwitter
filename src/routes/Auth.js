163073109eecf03e69dc2c7a3a8f1a73a111013c
Jiwoncoding <seojiwonba@gmail.com>
2022-12-11 21:37:00 +0900

    소셜 로그인 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { authService } from "../fbase";
+import { authService, firebaseInstance } from "../fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState(""); 
   const [password, setPassword] = useState(""); 
-  const [newAccount, setNewAccount] = useState(true); // eslint-disable-line no-unused-vars
-  const [error, setError] = useState(""); // eslint-disable-line no-unused-vars
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState(""); 
 
   const onChange = (event) => {
    const {
@@ -38,6 +38,21 @@ const Auth = () => {
 
   const toggleAccount = () => setNewAccount((prev)=>!prev);
 
+  const onSocialClick = async(event) => {
+    // console.log(event.target.value);
+    const {
+      target : { name },
+    } = event;
+    let provider;
+    if(name==="google"){
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if(name==="github"){
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
   return(
     <div>
       <form onSubmit={onSubmit}>
@@ -50,8 +65,8 @@ const Auth = () => {
         {newAccount ? "Sign In" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">Continue with Google</button>
+        <button onClick={onSocialClick} name="github">Continue with Github</button>
       </div>
     </div>
   );
